1f034ac1c02241d154408b8639fcd209e73e0d27
ewwjackson <ewartjackson@icloud.com>
2017-02-19 11:16:27 -0800

    add two functions

---
+++ printsum.js
@@ -0,0 +1,13 @@
+var print_sum=function() {
+sum = 0;
+for(i=0;i<=255;i++) {
+  console.log("New Number: " + i, "Sum :" + (sum += i));
+    }
+}
+
+print_sum();
+
+
+
+
+// routine functuons and methods are the samething. parameters aNd arguments are the samething. methods are attached to the object. libray pre complied
